7226156953aff6a1b863006bc15ed74a10f8502d
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-25 13:16:51 -0600

    Creacion del proyecto

---
+++ src/app/Componente/realizar-operacion/realizar-operacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realizar-operacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './realizar-operacion.component.html',
+  styleUrl: './realizar-operacion.component.css'
+})
+export class RealizarOperacionComponent {
+
+}
